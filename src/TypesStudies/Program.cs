7d400850452015fbcb22534d3c888bfc46b718a2
Leonardo Vita Milanez de Almeida <leonardo.vita0606@gmail.com>
2021-08-04 20:01:43 -0300

    working with ref and out

--- src/TypesStudies/Program.cs
+++ src/TypesStudies/Program.cs
@@ -7,7 +7,36 @@ namespace src
     {
         static void Main(string[] args)
         {
-            DateTime initialDate = new DateTime(2021, 8, 15,22,55,25);
+            //WorkingWithDatetime();
+
+            int monthlyWage = 1234, 
+                months = 12,
+                bonusRef = 0;
+            
+            string multipleReturnsString;
+            DateTime multipleReturnsDate;
+
+            //Passagem de parametro por valor
+            int yearlyWage = CalculateYearlyWage(monthlyWage, months);
+
+            Console.WriteLine($"Yearly Wage: {yearlyWage}\n");
+
+            //out nao precisa ser inicializado
+            int yearlyWageOut = CalculateYearlyWage(monthlyWage, months, out int bonusOut, out multipleReturnsString, out multipleReturnsDate);
+
+            Console.WriteLine($"out bonus: {bonusOut}\nyearly Wage Out: {yearlyWageOut}\n");
+            Console.WriteLine($"out String: {multipleReturnsString}\nOut Date: {multipleReturnsDate}\n");
+
+            //ref precisa ser inicializado
+            int yearlyWageRef = CalculateYearlyWageRef(monthlyWage, months, ref bonusRef);
+
+            Console.WriteLine($"ref bonus: {bonusRef}\nyearly Wage Ref: {yearlyWageRef}\n");
+
+        }
+
+        static void WorkingWithDatetime()
+        {
+            DateTime initialDate = new DateTime(2021, 8, 15, 22, 55, 25);
             var cultureInfo = new CultureInfo("en-US");
             string dateString = "1995/01/21";
             DateTime cultureDateTime = DateTime.Parse(dateString);
@@ -23,8 +52,22 @@ namespace src
 
             Console.WriteLine($"Culture Date: {cultureDateTime.ToLongDateString()}");
             Console.WriteLine($"Culture Date: {cultureInfo.Name}");
-
-            
+        }
+        public static int CalculateYearlyWage(int monthWage, int numberOfMonthWorked)
+        {
+            return monthWage * numberOfMonthWorked;
+        }        
+        public static int CalculateYearlyWage(int monthWage, int numberOfMonthWorked, out int bonus, out string multipleReturns, out DateTime multipleReturnsDate)
+        {
+            bonus = new Random().Next(1000);
+            multipleReturns = "com out podemos fazer multiplos retornos até mesmo em um metodo que retorno void";
+            multipleReturnsDate = DateTime.Now;
+            return monthWage * numberOfMonthWorked + bonus;
+        }
+        public static int CalculateYearlyWageRef(int monthWage, int numberOfMonthWorked, ref int bonus)
+        {
+            bonus = new Random().Next(1000);
+            return monthWage * numberOfMonthWorked + bonus;
         }
     }
 }
