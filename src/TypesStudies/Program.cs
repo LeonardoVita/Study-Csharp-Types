0adfe1813b9bafc6473b547ede095a0d3717ddaa
Leonardo Vita Milanez de Almeida <leonardo.vita0606@gmail.com>
2021-08-04 19:05:05 -0300

    working with datetime

--- src/TypesStudies/Program.cs
+++ src/TypesStudies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace src
 {
@@ -6,7 +7,24 @@ namespace src
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime initialDate = new DateTime(2021, 8, 15,22,55,25);
+            var cultureInfo = new CultureInfo("en-US");
+            string dateString = "1995/01/21";
+            DateTime cultureDateTime = DateTime.Parse(dateString);
+
+            Console.WriteLine(initialDate);
+            Console.WriteLine($"To Long Date: {initialDate.ToLongDateString()}");
+            Console.WriteLine($"To Long Time: {initialDate.ToLongTimeString()}");
+
+            Console.WriteLine($"To Short Date: {initialDate.ToShortDateString()}");
+            Console.WriteLine($"To Short Time: {initialDate.ToShortTimeString()}");
+
+            Console.WriteLine($"===============================================");
+
+            Console.WriteLine($"Culture Date: {cultureDateTime.ToLongDateString()}");
+            Console.WriteLine($"Culture Date: {cultureInfo.Name}");
+
+            
         }
     }
 }
